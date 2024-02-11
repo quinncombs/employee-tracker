a685636b00841f561356e04188a3706220a44792
quinncombs <qcombs31@gmail.com>
2024-02-11 18:37:54 -0500

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,22 +5,22 @@ VALUES  ('IT'),
         ('Engineering'),
         ('Finance');
 
-INSERT INTO roles (title, salary, deptId)
-VALUES  ('Manager', 80000, 2),
-        ('Social Media', 50000, 3),
-        ('Accountant', 70000, 5),
-        ('Head of Accounting', 9000, 5),
-        ('Software Engineer', 80000, 4),
-        ('IT Technician', 45000, 1),
-        ('Graphic Designer', 50000, 3),
-        ('Hardware Engineer', 80000, 4),
-        ('Assistant Manager', 60000, 2),
-        ('Head Engineer', 120000, 4);
+INSERT INTO roles (id, title, salary, deptId)
+VALUES  (1, 'Manager', 80000, 2),
+        (2, 'Social Media', 50000, 3),
+        (3, 'Accountant', 70000, 5),
+        (4, 'Head of Accounting', 9000, 5),
+        (5, 'Software Engineer', 80000, 4),
+        (6, 'IT Technician', 45000, 1),
+        (7, 'Graphic Designer', 50000, 3),
+        (8, 'Hardware Engineer', 80000, 4),
+        (9, 'Assistant Manager', 60000, 2),
+        (10, 'Head Engineer', 120000, 4);
 
-INSERT INTO employee (firstName, lastName, roleId, managerId)
-VALUES  ('Sasuke', 'Uchiha', 9, 6),
-        ('Kakashi', 'Hatake', 10, NULL),
-        ('Rock', 'Lee', 2, 4),
-        ('Ino', 'Yamanaka', 7, NULL),
-        ('Hinata', 'Hyuga', 5, 2),
-        ('Sakura', 'Haruno', 1, NULL)
+INSERT INTO employee (id, firstName, lastName, roleId, managerId)
+VALUES  (1, 'Sasuke', 'Uchiha', 9, 6),
+        (2, 'Kakashi', 'Hatake', 10, NULL),
+        (3, 'Rock', 'Lee', 2, 4),
+        (4, 'Ino', 'Yamanaka', 7, NULL),
+        (5, 'Hinata', 'Hyuga', 5, 2),
+        (6, 'Sakura', 'Haruno', 1, NULL)
